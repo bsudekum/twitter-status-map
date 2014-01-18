03bdeca5fb82559826c7b6af44d648a84b045197
rsudekum <bobby@mapbox.com>
2014-01-18 12:57:52 -0800

    First

---
+++ README.md
@@ -0,0 +1,8 @@
+Twitter Map
+========
+
+This plugin adds a little map below all geo-located tweets.
+
+Note: the map will only show up on a status page like, [twitter.com/bobws/status/424603651135389697](https://twitter.com/bobws/status/424603651135389697)
+
+![](https://i.cloudup.com/4OM268iFxH-2000x2000.png)
---
+++ twitter-status-map.js
@@ -0,0 +1,15 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+var loc = document.getElementsByClassName('tweet-geo-text');
+
+for (var i = 0; i < loc.length; i++) {
+    var lat = loc[i].href.split('%2C')[0].split('q=')[1];
+    var lng = loc[i].href.split('%2C')[1].split('&')[0];
+
+    var newImage = document.createElement('img');
+    newImage.src = '//api.tiles.mapbox.com/v3/twitter.map-prnk18sf/pin-m+55acee(' + lng + ',' + lat + ')/' + lng + ',' + lat + ',14/492x100.png';
+    newImage.style.margin = '6px 0 0'
+    insertAfter(loc[i], newImage);
+}
